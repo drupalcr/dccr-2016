dcdca8ceed2cd09925fd2fc568cac184b753a5c7
Andrés Díaz Soto <andres@estudiomanati.com>
2015-05-21 16:42:42 -0600

    Base theme files.

---
+++ sites/all/themes/dccr2015/templates/system/page.tpl.php
@@ -0,0 +1,111 @@
+<header class="header" role="banner">
+  <?php if ($messages): ?>
+  <div class="messages-wrapper">
+    <div class="messages-content">
+      <?php print $messages; ?>
+    </div>
+  </div>
+  <?php endif; ?>
+  <?php if ($page['utility_bar']): ?>
+    <div class="utility-bar">
+      <div class="utility-bar-content">
+        <?php print render($page['utility_bar']); ?>
+      </div><!-- /utility-bar-content -->
+    </div><!-- /utility-bar -->
+  <?php endif; ?>
+  <div class="header-content">
+
+    <?php if ($logo): ?>
+      <a href="<?php print $front_page; ?>" title="<?php print $site_name; ?>" class="logo" rel="home" id="logo">
+        <img src="<?php print $logo; ?>" alt="<?php print $site_name; ?>" />
+      </a>
+    <?php endif; ?>
+
+    <?php if ($site_name || $site_slogan): ?>
+
+        <?php if ($site_name): ?>
+          <?php if ($title): ?>
+
+            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+
+          <?php else: /* Use h1 when the content title is empty */ ?>
+            <h1 id="site-name">
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+            </h1>
+          <?php endif; ?>
+        <?php endif; ?>
+
+        <?php if ($site_slogan): ?>
+          <?php print $site_slogan; ?>
+        <?php endif; ?>
+
+    <?php endif; ?>
+
+    <?php print render($page['header']); ?>
+
+  </div><!-- /header-content -->
+</header>
+
+<?php if ($page['above_content']): ?>
+  <section class="above-content">
+    <?php print render($page['above_content']); ?>
+  </section>
+<?php endif; // end Above Content ?>
+
+<div class="main-content">
+  <?php if ($breadcrumb): ?>
+    <!-- <?php print $breadcrumb; ?> -->
+  <?php endif; ?>
+
+  <?php if ($page['highlighted']): ?>
+    <?php print render($page['highlighted']); ?>
+  <?php endif; ?>
+
+    <a id="main-content"></a>
+    <div class="main" role="main">
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+      <?php print render($title_suffix); ?>
+
+      <?php if ($tabs): ?>
+        <?php print render($tabs); ?>
+
+      <?php endif; ?>
+
+      <?php print render($page['help']); ?>
+
+      <?php if ($action_links): ?>
+        <ul class="action-links">
+          <?php print render($action_links); ?>
+        </ul>
+      <?php endif; ?>
+
+      <?php print render($page['content']); ?>
+
+    </div>
+
+  <?php if ($page['sidebar']): ?>
+    <div id="sidebar" class="sidebar">
+      <?php print render($page['sidebar']); ?>
+    </div> <!-- /.section, /#sidebar -->
+  <?php endif; ?>
+
+</div>
+
+<?php if ($page['below_content']): ?>
+  <section class="below-content">
+    <?php print render($page['below_content']); ?>
+  </section>
+<?php endif; // end Below Content ?>
+
+<footer class="footer" role="contentinfo">
+  <div class="footer-content">
+    <?php print render($page['footer']); ?>
+  </div>
+</footer>
+
+<?php if ($page['closure']): ?>
+<aside class="closure">
+  <?php print render($page['closure']); ?>
+</aside>
+<?php endif; // end closure ?>
